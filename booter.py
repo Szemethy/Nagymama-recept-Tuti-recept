41fb608253994662ce732e625c04e476dcd7e370
theszadukogames <66076152+theszadukogames@users.noreply.github.com>
2022-11-08 22:26:12 +0100

    menu, booter and frontend (for console format)

---
+++ booter.py
@@ -0,0 +1,42 @@
+from frontend import *
+import random
+import time
+import os
+
+wisedomList = []
+
+def getBooterText():
+    with open("wisdom.txt","r",encoding="UTF-8") as file:
+        for row in file:
+            wisedomList.append(row.strip())
+
+def boot():
+    os.system('cls')
+
+    text("bold")
+    print("Betöltés, kérem várjon!",end="")
+    text("end")
+
+    minLoad = 6
+    maxLoad = 14
+    pointList = "." * random.randint(minLoad,maxLoad)
+
+    #[pointList.append(".") for x in range(minLoad,maxLoad)]
+    slowPrint(pointList,0.8)
+    text("green","bold")
+    print("KÉSZ!")
+    time.sleep(1)
+
+    os.system('cls')
+    text("end")
+    print("\t\t ",end="")
+
+    text("italic","red")
+    randomInListIndex = random.randint(0,len(wisedomList)-1)
+    slowPrint(f'"{wisedomList[randomInListIndex]}"',0.05)
+    text(Format.END)
+
+    slowPrint("\n\n",0.8)
+
+
+
