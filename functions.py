705294008ab54cf9fe48976826891632b6d6bb85
Bende <bende.huba@studenst.jedlik.eu>
2022-11-23 13:51:12 +0100

    main.py fixes

--- functions.py
+++ functions.py
@@ -14,22 +14,11 @@ def readFile():
     f.close()
 
 def writeFile():
-    f = open('etelek.csv', 'w', encoding='UTF-8')
-    for r in results:
-        row = f'{r.name};{r.time};{r.difficult}\n'
-        f.write(row)
-    f.close()
-
-def modifyResult():
-    name = input('Név: ')
-    for r in results:
-        if r.name.lower() == name.lower():
-            # index = results.index(r)     # r elem indexe a results listában
-            r.time = input('Idő ( perc): ')
-            r.difficult = input('Nehézség (könnyű, közepes, bonyolult): ')       
-            writeFile()
-            return
-    input('Ilyen nevű étel nincs az adatbázisban')
+    with open('etelek.csv', 'w', encoding='UTF-8') as f:
+        for r in results:
+            print(r)
+            row = f'{r.name};{r.time};{r.difficult};{r.preparation};{r.ingredients}\n'
+            f.write(row)
 
 def newResult():
     name = input('Adja meg a nevét: ')
@@ -39,13 +28,19 @@ def newResult():
     ing = input('Hozzávalók: ') 
 
     row = f'{name};{time};{difficult};{prep};{ing}\n'
+    return row, [name,time,difficult,prep,ing]
+
+def addResultToEnd(row):
     with open('etelek.csv', 'a', encoding='UTF-8') as f:
         f.write(row)
 
     r = Result(row)
     results.append(r)
 
-
+def changeResultById(id:int,rowlist:list):
+    results[id] = rowlist
+    writeFile()
+    
 
 def smallText(i,r):
     #randomColor()
@@ -72,15 +67,17 @@ def searchByName():
     indexList = []
     for i,r in enumerate(results):
         if name.lower() in r.name.lower():
-            smallText(count,r)
+            #smallText(count,r)
             count += 1
             indexList.append(i)
     if count == 0:
-        return False
+        return indexList,False
     elif count == 1:
-        allDataByNameOrIndex(nameOrIndex=indexList[0],isByIndex=True)
-        return 1
-    return indexList
+        return indexList,"1"
+
+    for i,r in enumerate(indexList):
+        smallText(i,results[r])
+    return indexList,True
 
 '''
 def nameTimeDif():
